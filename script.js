0675bdf16fb859fc8568645d84781bc56dac2308
Monark773 <serebeaufils7@gmail.com>
2022-09-03 09:50:28 -0300

    portfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+const menu =document.getElementById("menu-bars");
+const header = document.querySelector("header");
+
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+    console.log(header.classList);
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let cursor1 = document.querySelector(".cursor-1");
+let cursor2 = document.querySelector(".cursor-2");
+
+window.onmousemove = (e) => {
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+let links = document.querySelectorAll('a').forEach(links =>{
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+})
